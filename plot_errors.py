be080d7bb638bf279c7e9f676422a414ad732631
kevin <kevineslee56@gmail.com>
2023-10-20 15:07:12 -0400

    2nd plot rework

--- plot_errors.py
+++ plot_errors.py
@@ -12,26 +12,35 @@ def plot_errors(filename):
     
     fig, axes = plt.subplots(1,2, figsize=(14,6))
 
+    x_labels = []
+    y_labels = []
     label_units = []
     title_prefix = ""
     if filename == "linear.csv":
         title_prefix = "Linear Error: "
-        label_units.append(["[m]", "[m/s]"])
-        label_units.append(["[m]", "[m/s]", ""])
+        label_units = ["[m]", "[m/s]", ""]
+        x_labels = ["e [m]", "Time [ns]"]
+        y_labels = ["e_dot [m/s]", "Errors"]
     elif filename == "angular.csv":
         title_prefix = "Angular Error: "
-        label_units.append(["[m]", "[m/s]"])
-        label_units.append(["[m]", "[m/s]", ""])
+        label_units = ["[m]", "[m/s]", ""]
+        x_labels = ["e [m]", "Time [ns]"]
+        y_labels = ["e_dot [m/s]", "Errors"]
     elif filename == "robot_pose.csv":
         title_prefix = "Robot Pose: "
-        label_units.append(["[m]", "[m]"])
-        label_units.append(["[m]", "[m]", "[rad]"])
+        label_units = ["[m]", "[m]", "[rad]"]
+        x_labels = ["x [m]", "Time [ns]"]
+        y_labels = ["y [m/s]", "Pose Measurements"]
 
     axes[0].plot([lin[0] for lin in values], [lin[1] for lin in values])
     axes[0].set_title(title_prefix + "State Space")
+    axes[0].set_xlabel(x_labels[0])
+    axes[0].set_ylabel(y_labels[0])
     axes[0].grid()
 
     axes[1].set_title(title_prefix + "Each Individual State")
+    axes[1].set_xlabel(x_labels[1])
+    axes[1].set_ylabel(y_labels[1])
     for i in range(0, len(headers) - 1):
         axes[1].plot(time_list, [lin[i] for lin in values], label = headers[i] + " " + label_units[i])
 
